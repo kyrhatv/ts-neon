3837e7eba965386cedd5878eeef8815eac73b28d
JeanMichelLaforge1 <jean-michel.laforge@timesphere.com>
2020-04-17 12:12:27 -0400

    finished new app layout :)

--- src/hs-components/MainMenuSidebar/MainMenu.tsx
+++ src/hs-components/MainMenuSidebar/MainMenu.tsx
@@ -1,19 +1,14 @@
 import React from 'react';
 
 import { FunctionComponent } from 'react';
-
 import SideBar from '../sf-sidebar';
 import IconBar from './IconBar';
-
 import { NavStruct } from '../../app-main/utils/RootStructInterface';
 import { MenuContent } from './MenuContent/MenuContent';
 import { usePrevious } from '../../hs-utils/hs-hooks';
-
 import { useSelector, useDispatch } from 'react-redux';
-
 import { selectById, updateMenu } from '../sf-sidebar/menusSlice';
 import { RootState } from '../../app-main/app/store';
-
 import { MAIN_MENU_ID } from '../../hs-utils/constants/constants';
 
 export const MainMenu: FunctionComponent<NavStruct> = ({ struct }) => {
@@ -22,7 +17,7 @@ export const MainMenu: FunctionComponent<NavStruct> = ({ struct }) => {
 
     const currentModule = menuState.currentModule;
     const prevModule = usePrevious(currentModule);
-
+    
     const isShown = prevModule === currentModule && menuState.isShown === true ? false : menuState.isShown;
 
     const pinChangedHandler = () => {
